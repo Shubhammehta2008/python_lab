9bf0248ac54d8ac650225daf9721cfbcd72fe7db
Shubham Mehta <shubhammehtajain2008@gmail.com>
2026-09-14 15:44:31 +0530

    Refactor find_small_largest.py for clarity and correctness

--- find_small_largest.py
+++ find_small_largest.py
@@ -1,11 +1,9 @@
 number=[2,3,4,5,6,7,8,9]
-smallest=number[0]
-largest=number[0]
- for i in range 8:
-    if(number[i]<smallest)smallest=number[i]
-    if(number[i>largest]) largest=number[i]
+small=number[0]
+large=number[0]
+for i in number:
+    if(i<small):small=i
+    if(i>large): large=i
 
-print(smallest)
-print(largest)
-
-
+print(small)
+print(large)
